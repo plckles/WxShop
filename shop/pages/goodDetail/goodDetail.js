462d31855320531f4fbd11936862b20cc4dff0de
baozi <18473608506@163.com>
2018-11-22 17:34:03 +0800

    tests sdsds

---
+++ shop/pages/goodDetail/goodDetail.js
@@ -0,0 +1,74 @@
+// pages/goodDetail/goodDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: true, //是否自动切换 
+    interval: 3000, //自动切换时间间隔 
+    duration: 1000, //滑动动画时长 
+    imgUrls:[
+      '../../image/1.jpg',
+      '../../image/8.jpg',
+      '../../image/10.jpg'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
