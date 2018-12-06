58b7433d29a676d47564f34e3cadfc8eb5a1ce77
baozi <18473608506@163.com>
2018-12-06 09:43:19 +0800

    新增登录注册，忘记密码

---
+++ shop/pages/login/login.js
@@ -0,0 +1,89 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  goApply: function () {
+    wx.navigateTo({
+      url: '../apply/apply'
+    });
+  },
+  goYanzheng: function () {
+    wx.navigateTo({
+      url: '../password/password'
+    });
+  },
+  sublogin: function (e) {
+    var mobile = e.detail.value.mobile;
+    var password = e.detail.value.password;
+    if (!(mobile && password)) {
+      wx.showModal({
+        title: '友情提示',
+        content: '数据不能为空',
+      })
+      return false;
+    }
+
+
+  }
+})
---
+++ shop/pages/password/password.js
@@ -0,0 +1,147 @@
+// pages/password/password.js
+var interval = null //倒计时函数
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    phone: '',
+    authcode: '',
+    codename: '获取验证码',
+    crrentTime: 60,//限制倒计时60秒
+    isClick: false,//获取验证码按钮，默认允许点击
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //手机，验证码
+  getPhone: function (e) {
+    //console.log(e.detail.value)
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  getAuthcodeInput: function (e) {
+    // console.log(e.detail.value)
+    this.setData({
+      authcode: e.detail.value
+    })
+  },
+  getAuthcode: function () {
+    let that = this;
+    //验证手机
+    var myreg = /^(14[0-9]|13[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$$/;//正则
+    //console.log(that.data.phone)
+    if (that.data.phone.length == 0) {
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none',
+        duration: 1000
+      })
+      return false;
+    }
+    if (!myreg.test(that.data.phone)) {
+      wx.showToast({
+        title: '错误的手机号',
+        icon: 'none',
+        duration: 1000
+      })
+      return false;
+    }
+
+    that.setData({
+      isClick: true,
+    })
+
+    // 60s倒计时 setInterval功能用于循环，常常用于播放动画，或者时间显示
+    var currentTime = that.data.crrentTime;
+    interval = setInterval(function () {
+      currentTime--;//减
+      that.setData({
+        codename: currentTime + '秒后获取'
+      })
+      if (currentTime <= 0) {
+        clearInterval(interval)
+        that.setData({
+          codename: '获取验证码',
+          currentTime: 60,
+          isClick: false
+        })
+      }
+    }, 1000);
+
+  },
+  submit: function (e) {
+    var password = e.detail.value.password;
+    var password2 = e.detail.value.password2;
+    var phones = e.detail.value.phones;
+    var code = e.detail.value.code;
+    if (!(phones && code && password && password2)) {
+      wx.showToast({
+        title: '存在未填写字段',
+      })
+    } else if (password != password2) {
+      wx.showToast({
+        title: '两次密码不一致',
+      })
+    }
+    else {
+
+
+    }
+  }
+})
